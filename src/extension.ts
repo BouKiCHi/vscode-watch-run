fa43ffd2dfc9351f4438eaf92f86683bc0b61316
BouKiCHi <alf_bincyou@hotmail.com>
2018-10-27 14:09:35 +0900

    added multi-root support. added internal command support.

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 // import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-
+    // console.log("Activating...");
     var chokidar = require('chokidar');
 
     let config = vscode.workspace.getConfiguration('watch-run');
@@ -11,8 +11,8 @@ export function activate(context: vscode.ExtensionContext) {
         return;
     }
 
-    let rootPath : string = vscode.workspace.rootPath || "";
-    if (!rootPath) {
+    const folders = vscode.workspace.workspaceFolders;
+    if (typeof folders === 'undefined') {
         return;
     }
 
@@ -21,18 +21,42 @@ export function activate(context: vscode.ExtensionContext) {
         return;
     }
 
-    for(var i=0; i < targetList.length; i++) {
-        function taskFunction(target:string,task:string) {
+    function setWorkspace(workspacePath:string) {
+        // console.log("Adding:" + workspacePath);
+
+        function addWatchTarget(target:string, task:string , command: string) {
             chokidar.watch(target, {
-                cwd: rootPath
+                cwd: workspacePath
             }).on('change', (filename:string, stats:object) => {
                 // let targetFile = path.join(rootPath ? rootPath : "",filename);
                 // console.log("File:" + targetFile + " Task:" + task);
                 // console.log("filename:" + filename);
+                if (task === undefined) {
+                    vscode.commands.executeCommand(command);
+                }
                 vscode.commands.executeCommand("workbench.action.tasks.runTask",task);
-          });
+            });
         }
-        taskFunction(targetList[i]['target'],targetList[i]['task']);
+
+        for(var i=0; i < targetList.length; i++) {
+            let target = targetList[i]['target'];
+            let task = targetList[i]['task'];
+            let command = targetList[i]['command'];
+            
+            if (target === undefined) { continue; }
+            addWatchTarget(target,task,command);
+        }
+    }
+
+    // important!
+    // vscode.workspace.rootPath(in windows) format is : 'c:\folder\folder'
+    // 
+    // let rootPath = vscode.workspace.rootPath || "";
+    // setWorkspace(rootPath);
+
+
+    for(var i=0; i < folders.length; i++) {
+        setWorkspace(folders[i].uri.fsPath);
     }
 }
 
